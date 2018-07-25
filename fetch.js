ec2c2c9bfe95691ac5e19c6a1c61d32092eb3f91
Michael Nthiwa <mike.nthiwa@gmail.com>
2018-07-25 13:04:51 +0300

    Adding boolean value to driver checkbox

--- fetch.js
+++ fetch.js
@@ -1,19 +1,24 @@
 document.getElementById('signUpPassenger').addEventListener('submit', register);
 
-
 function register(e) {
     e.preventDefault();
 
    let username = document.getElementById('username').value;
    let email = document.getElementById('email').value;
    let password = document.getElementById('password').value;
+    if (document.getElementById('isDriver').checked === true){
+        is_driver = document.getElementById('isDriver').value = true
+    }
+    else {
+        is_driver = document.getElementById('isDriver').value = false
+    }
 
 
 fetch('http://127.0.0.1:5000/api/v3/auth/signup', {
     method: 'POST',
     mode: 'cors',
     headers: {'Content-Type': 'application/json'},
-    body: JSON.stringify({"username": username, "email": email, "password": password})
+    body: JSON.stringify({"username": username, "email": email, "password": password, "is_driver": is_driver})
 })
     .then((res) => res.json())
     .then(data => {
